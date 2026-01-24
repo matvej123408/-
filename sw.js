954c117992c404d0c9c8e0e2cd59f3b35038c925
matvej123408 <matvejbojko146@gmail.com>
2026-01-24 10:44:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('ar-distance-cache').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './app.js',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
